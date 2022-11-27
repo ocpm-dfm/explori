0bb10eb08c4a84d686d5574126e92437944a676f
ChiragTutlani <chirag161198@gmail.com>
2022-11-27 17:37:46 +0100

    frontend for upload event log

--- frontend/src/components/Session/Session.tsx
+++ frontend/src/components/Session/Session.tsx
@@ -1,11 +1,68 @@
-import React from 'react';
 import './Session.css';
-import {DefaultLayout} from "../DefaultLayout/DefaultLayout";
+import { ExploriNavbar } from '../ExploriNavbar/ExploriNavbar';
+import { ObjectSelection } from '../ObjectSelection/ObjectSelection';
+import { useEffect, useState } from 'react';
 
 export function Session(_props: any) {
-    const content = (
-        "This is the page to initialize a new session."
-    );
+    const initialSelectedFile: object[] = []
+    const [selectedFile, setSelectedFile] = useState(initialSelectedFile)
+    const [isFileSelected, setIsFileSelected] = useState(false)
+    const objectSelection = <ObjectSelection />
 
-    return <DefaultLayout content={content} />
+    useEffect(() => {
+        setIsFileSelected(
+            selectedFile.length >= 1 ? true : false
+        )
+    }, [selectedFile])
+
+    const handleFileSelection = async (event: any) => {
+        event.preventDefault()
+
+        setSelectedFile((oldFiles) => {
+            const newFiles = event.target.files
+            return [...oldFiles, ...newFiles]
+        })
+    }
+
+    const uploadFile = async (event: any) => {
+        event.preventDefault()
+        //TODO: send files to backend 
+    }
+
+    return (
+        <div className="Session">
+            <ExploriNavbar lowerRowSlot={objectSelection} />
+            <form id="uploadEventLogForm">
+                <label htmlFor="uploadEventttLog">Upload an event log:</label>
+                <input
+                    type="file"
+                    accept=".jsonocel, .xmlocel"
+                    name="uploadEventttLog"
+                    onChange={handleFileSelection}
+                    multiple >
+                </input>
+                {
+                    isFileSelected ?
+                        <div>
+                            <div>Files ready to be uploaded:</div>
+                            <div>{
+                                selectedFile.map((file: any, index: number) => {
+                                    return (
+                                        <div key={index.toString()}>
+                                            {file.name}
+                                        </div>
+                                    )
+                                })
+                            }</div>
+                        </div> : <div></div>
+                }
+                <button
+                    form='uploadEventLogForm'
+                    disabled={false}
+                    onClick={uploadFile} >
+                    Confirm Upload
+                </button>
+            </form>
+        </div>
+    )
 }
