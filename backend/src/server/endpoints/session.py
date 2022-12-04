ad0ad1746eae683c3f6930949126a037bdfda1ef
Vincent Ehrmanntraut <vinc0682@outlook.de>
2022-12-04 11:25:19 +0100

    Fixed sessions backend

--- backend/src/server/endpoints/session.py
+++ backend/src/server/endpoints/session.py
@@ -1,73 +1,63 @@
+import json
 import os
+from typing import List, Tuple
 
-from fastapi import APIRouter, UploadFile
-from fastapi.responses import FileResponse
-from typing import List, Dict
-
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from starlette import status
 
 router = APIRouter(prefix="/session", tags=['Session management'])
 
 
-class AvailableSessionsResponseModel(BaseModel):
-    __root__: List[List[str]]
+SESSIONS_FOLDER = os.path.join("cache", "sessions")
+os.makedirs(SESSIONS_FOLDER, exist_ok=True)
+
+
+class Session(BaseModel):
+    base_ocel: str
+    threshold: float
 
     class Config:
         schema_extra = {
-            "example": [
-                "session1",
-                "session2"
-            ]
+            "example": {
+                "base_ocel": "uploaded/p2p-normal.jsonocel",
+                "threshold": 0.8
+            }
         }
 
 
-class RestoreResponseModel(BaseModel):
-    __root__ = Dict[str, (str | FileResponse)]
+class StoreSessionPayload(BaseModel):
+    name: str
+    session: Session
 
 
-class StoreSessionResponseModel(BaseModel):
-    __root__: Dict[str, bool]
-
-    class Config:
-        schema_extra = {
-            "example": [
-                "session1",
-                "session2"
-            ]
-        }
-
-
-class RestoreSessionResponseModel(BaseModel):
-    __root__: List[List[str]]
-
-    class Config:
-        schema_extra = {
-            "example": [
-                "session1",
-                "session2"
-            ]
-        }
-
-
-@router.put('/store', response_model=StoreSessionResponseModel)
-async def upload_session_file(file: UploadFile):
-    file_location = "/cache/sessions/" + file.filename
-    file_content = await file.read()
-    with open(file_location, "wb") as f:
-        f.write(file_content)
-
-
-
+@router.put('/store')
+async def store_session(payload: StoreSessionPayload):
+    session_file = get_session_file(payload.name)
+    with open(session_file, 'w') as f:
+        json.dump(payload.session.dict(), f)
     return {'status': 'successful'}
 
 
-@router.get('/restore', response_model=RestoreSessionResponseModel)
-def restore_session(file_name: str) -> Dict[str, (str | FileResponse)]:
-    file_location = "/cache/sessions" + file_name
-    return {
-        'status': 'successful',
-        'file': FileResponse(file_location)
-    }
+@router.get('/restore', response_model=Session)
+def restore_session(name: str) -> Session:
+    session_file = get_session_file(name)
+    if not os.path.isfile(session_file):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unkown session.")
+
+    with open(session_file, 'r') as f:
+        return Session(**json.load(f))
+
+
+class AvailableSessionsResponseModel(BaseModel):
+    __root__: List[Tuple[str, float]]
+
+    class Config:
+        schema_extra = {
+            "example": [
+                ["default", 1670149085]
+            ]
+        }
 
 
 @router.get('/available', response_model=AvailableSessionsResponseModel)
@@ -77,30 +67,22 @@ def list_available_sessions():
         API endpoints.
     """
 
-    def find_available_sessions(folder: str) -> List[str]:
-        result = []
-        for node in os.listdir(folder):
-            complete_path = os.path.join(folder, node)
-            if os.path.isfile(complete_path) and (node.endswith(".json")):
-                result.append(os.path.join(folder, node))
-            elif os.path.isdir(complete_path):
-                result.extend(find_available_sessions(complete_path))
-        return result
+    if not os.path.isdir(SESSIONS_FOLDER):
+        return []
 
-    # Cut of the data/ from the log names
-    def cut_filename(filename: str):
-        return filename[15:]
+    result = []
+    for file in os.listdir(SESSIONS_FOLDER):
+        if file.endswith(".json"):
+            session_name = file[:-5]
+            last_change = os.path.getmtime(os.path.join(SESSIONS_FOLDER, file))
+            result.append((session_name, last_change))
+    return result
 
-    def extend_result_by_date(session: List[str]):
-        results = []
-        for entry in session:
-            results.append([cut_filename(entry), os.path.getmtime(entry)])
-        return results
 
-    # Find all logs in the data folder.
-    sessions = find_available_sessions("cache/sessions")
-
-    # Extend results by file size
-    extended_logs = extend_result_by_date(sessions)
-
-    return extended_logs
+def get_session_file(session_name: str):
+    """Determines the file to store the session to. Prevents path traversals."""
+    session_file_unvalidated = os.path.join(SESSIONS_FOLDER, session_name + ".json")
+    session_file = os.path.abspath(session_file_unvalidated)
+    if session_file[-len(session_file_unvalidated):] != session_file_unvalidated:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Path traversals are not allowed!")
+    return session_file
