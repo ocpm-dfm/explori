5930f01d232e8d3448345d73d8ac32ddea6fc5bd
Jan Brinkmann <jan.brinkmann@rwth-aachen.de>
2022-12-05 11:51:19 +0100

    Revert "Merge branch 'UserSession' into 'main'"
    This reverts commit 44ced4b27484d5f36873efc883cc8a8ea5d054a6, reversing
    changes made to bf6834908c5d4ee646352e219397287a7172330b.

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -10,9 +10,72 @@ import {useAsyncAPI} from "../../api";
 import {selectedObjectTypesUpdateCallback} from "../ObjectSelection/ObjectSelection";
 import {MultiValue} from "react-select";
 
-export const Home = (props: { filteringThreshold: number, stateChangeCallback: any}) => {
-    const filteringThreshold = props.filteringThreshold;
-    const stateChangeCallback = props.stateChangeCallback;
+type HomeState = {
+    threshold: number,
+    selectedObjectTypes: string[],
+}
+
+function Home(props: {ocel: string}) {
+    const [state, setState] = useState<HomeState>({
+        threshold: 100,
+        selectedObjectTypes: [],
+    })
+
+    const location = useLocation();
+
+    // const dfm: DirectlyFollowsMultigraph = {
+    //     nodes: [
+    //         {
+    //             label: "First activity",
+    //             threshold: 0
+    //         },
+    //         {
+    //             label: "Optional activity, only for obj-t-1",
+    //             threshold: 0.5
+    //         },
+    //         {
+    //             label: "Third activity",
+    //             threshold: 0
+    //         }
+    //     ],
+    //     subgraphs: {
+    //         "obj-t-1": [
+    //             {
+    //                 source: 0,
+    //                 target: 2,
+    //                 threshold: 0
+    //             },
+    //             {
+    //                 source: 0,
+    //                 target: 1,
+    //                 threshold: 0.5,
+    //             },
+    //             {
+    //                 source: 1,
+    //                 target: 2,
+    //                 threshold: 0.5
+    //             }
+    //         ],
+    //         "obj-t-2": [
+    //             {
+    //                 source: 0,
+    //                 target: 2,
+    //                 threshold: 0
+    //             }
+    //         ]
+    //     }
+    // };
+
+    const updateCallback: selectedObjectTypesUpdateCallback = function(selection: string[]) {
+        setState((old) => Object.assign({}, old, {
+            selectedObjectTypes: selection,
+        }));
+    }
+    
+    // This command clears the location.state which is saved even upon refreshing the browser
+    // So for development, uncomment this line so the location gets cleared on each re-render. Can also lead to unwanted side-effects!
+    // For production, this makes sense I guess?
+    // window.history.replaceState({}, document.title)
 
     const dfm_query = useAsyncAPI<DirectlyFollowsMultigraph>("/pm/dfm",
         location.state === null || location.state === undefined? {ocel: 'uploaded/p2p-normal.jsonocel'} : location.state
@@ -25,19 +88,18 @@ export const Home = (props: { filteringThreshold: number, stateChangeCallback: a
         <React.Fragment>
             <div className="Home">
                 <ExploriNavbar lowerRowSlot={objectSelection} />
-                <FilteredDFM dfm={dfm_query.result} threshold={filteringThreshold / 100} selectedObjectTypes={selectedObjectTypes} />
+                <FilteredDFM dfm={dfm_query.result} threshold={state.threshold / 100} selectedObjectTypes={state.selectedObjectTypes} />
                 <div className="Home-DetailSlider">
                     <div className="Home-DetailSlider-Label">
                         Less detail
                     </div>
                     <input type="range" min="0" max="100"
                         className="Home-DetailSlider-Slider"
-                        value={filteringThreshold} onInput={(e) => {
-                            stateChangeCallback({
-                                filteringThreshold: (e.target as HTMLInputElement).value
-                            })
-                        }}
-                    />
+                        value={state.threshold} onInput={(e) => {
+                        setState((old) => {
+                            return Object.assign({}, old, {threshold: (e.target as HTMLInputElement).value})
+                        })
+                    }}/>
                     <div className="Home-DetailSlider-Label">
                         More detail
                     </div>
@@ -46,3 +108,5 @@ export const Home = (props: { filteringThreshold: number, stateChangeCallback: a
         </React.Fragment>
     );
 }
+
+export default Home;
