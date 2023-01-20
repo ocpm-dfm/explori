42017d6061b82b744d7beda13776986f35169513
Linus Detro <linus.detro@gmail.com>
2023-01-20 14:13:54 +0100

    Add dialog/disclaimer to panic button

--- frontend/src/pages/Help/Help.tsx
+++ frontend/src/pages/Help/Help.tsx
@@ -1,10 +1,14 @@
 import {ExploriNavbar} from "../../components/ExploriNavbar/ExploriNavbar";
 import  "../../components/DefaultLayout/DefaultLayout.css";
 import "./Help.css";
-import React from "react";
+import React, {useState} from "react";
 import {Button} from "@mui/material";
 import {getURI} from "../../hooks";
-import {Link} from "react-router-dom";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Dialog from "@mui/material/Dialog";
 
 async function clearCache(){
     const uri = getURI("/logs/clear_cache", {});
@@ -22,14 +26,41 @@ interface Props {
 }
 
 export function Help(props: Props) {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <div className="DefaultLayout-Container">
             <ExploriNavbar />
             <div className={"Help"}>
-                <Button onClick={clearCache} sx={{'background-color': 'red', 'color': 'white'}}>
+                <Button onClick={() => setOpen(true)} sx={{'background-color': 'red', 'color': 'white'}}>
                     Panic button
                 </Button>
             </div>
+            <Dialog
+                open={open}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Do you really want to clear the whole cache?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                            <p>
+                                This button completely clears the long term cache (on your drive) and also the redis cache containing tasks. Once deleted, they can't be restored
+                                and for each OCEL all computations need to be redone. The purpose of this button is to try to trouble-shoot if something has gone seriously wrong.
+                            </p>
+                            <p>
+                                Only press yes, if you know what you are doing.
+                            </p>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} autoFocus>No</Button>
+                    <Button onClick={clearCache}>Yes</Button>
+                </DialogActions>
+            </Dialog>
             <Button onClick={props.resetQueryState}>Clear queries</Button>
             <Button onClick={() => {
                 // set to demo ocel
