8e96a6b059b3e8400cfd1ec00856290e7a9a0fca
Jan Brinkmann <jan.brinkmann@rwth-aachen.de>
2023-01-23 18:29:22 +0100

    Add `testUserSession` to jest tests (simple call currently)

--- frontend/src/redux/store.test.ts
+++ frontend/src/redux/store.test.ts
@@ -1,5 +1,8 @@
 import { saveUserSession, createUserSession, modifyUserSession, restoreUserSession } from "./UserSession/userSession.actions";
 import { CREATE_USER_SESSION, SAVE_USER_SESSION, RESTORE_USER_SESSION, NO_CHANGE_USER_SESSION, UPDATE_USER_SESSION } from "./UserSession/userSession.types";
+import {render, screen} from "@testing-library/react";
+import React from "react";
+import store from "./store";
 
 export const testUserSession = (store: any) => {
     store.dispatch(restoreUserSession("uploaded/p2p-normal.jsonocel"))
@@ -20,3 +23,7 @@ export const testUserSession = (store: any) => {
     store.dispatch(saveUserSession(store.getState().session))
 
 }
+
+test('tests user session (no crash)', () => {
+    testUserSession(store);
+});
