b6911570a6b3d5031446b28e60ba9fad5f45f2d4
Jan Brinkmann <jan.brinkmann@rwth-aachen.de>
2023-02-09 06:10:31 +0100

    Move delete redis tasks helper script to utils and add redis connection details being read from env

--- backend/utils/delete_tasks_in_redis.py
+++ backend/utils/delete_tasks_in_redis.py
@@ -1,8 +1,11 @@
+import os
 from redis.client import Redis
 
+REDIS_HOST = os.environ.get('EXPLORI_REDIS_HOST', default='localhost')
+REDIS_PORT = os.environ.get('EXPLORI_REDIS_PORT', default='6379')
 
 def run():
-    redis = Redis('localhost', 6379)
+    redis = Redis(REDIS_HOST, REDIS_PORT)
     for key in redis.scan_iter("*ocel*"):
         redis.delete(key)
         print(f"Deleted {key}")
